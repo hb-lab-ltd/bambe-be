819fb1184f92af6588293564c66378bff883685b
juleshb <habaruremajules@gmail.com>
2024-12-31 12:55:09 +0200

    change view product to addmin

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,9 +4,9 @@ const productsController = require('../controllers/productsController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', productsController.getAllProductsWithImages);
-router.get('userproduct/', authMiddleware.authenticate, productsController.getAdminProducts);
+router.get('/userproduct', authMiddleware.authenticate, productsController.getAdminProducts);
 router.post('/',authMiddleware.authenticate, productsController.createProduct);
-router.get('/:id', productsController.getProductById); 
+router.get('product/:id', productsController.getProductById); 
 router.put('/:id', productsController.updateProduct); 
 router.delete('/:id', productsController.deleteProduct); 
 router.get('/single/:productId', productsController.getProductWithImages);
